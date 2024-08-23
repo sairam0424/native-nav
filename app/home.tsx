5560b95ffc13a555ac2dc765774e07602d18dbf4
sairam0424 <uggesairam0000@gmail.com>
2024-08-23 12:17:18 +0530

    returning the data to prev Screen using Stack

--- app/home.tsx
+++ app/home.tsx
@@ -1,23 +1,50 @@
-import React from "react";
-import { Button, Text, View } from "react-native";
+import { useEffect } from "react";
+import { Text, View, StyleSheet, Platform, Button } from "react-native";
 
-function Details({ navigation }) {
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    ...Platform.select({
+      ios: {
+        backgroundColor: "red",
+      },
+      android: {
+        backgroundColor: "green",
+      },
+      default: {
+        // other platforms, web for example
+        backgroundColor: "white",
+      },
+    }),
+  },
+});
+
+export default function Home({ navigation, route }) {
+  useEffect(() => {
+    if (route.params?.post) {
+    }
+  }, [route.params?.post]);
+
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Details Screen</Text>
-      <Button
-        title="Go to Details... again"
-        onPress={() => navigation.push("Details")}
-      />
+    <View style={styles.container}>
+      <Text>Home screen</Text>
+      <Text style={{ margin: 10 }}>Post: {route.params?.post}</Text>
 
-      <Button title="Go to Home" onPress={() => navigation.navigate("Home")} />
-      <Button title="Go back" onPress={() => navigation.goBack()} />
       <Button
-        title="Go back to first screen in stack"
-        onPress={() => navigation.popToTop()}
+        title="Go to Details"
+        onPress={() =>
+          navigation.navigate("Details", {
+            itemId: 86,
+            otherParam: "anything you want here",
+          })
+        }
+      />
+      <Button
+        title="Create post"
+        onPress={() => navigation.navigate("CreatePost")}
       />
     </View>
   );
 }
-
-export default Details;
--- app/index.tsx
+++ app/index.tsx
@@ -4,7 +4,8 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
 import Home from "./home";
 import Details from "./screens/details";
-
+// import CreatePostScreen from "./screens/createPost";
+import CreatePostScreen from "./screens/createPost";
 const router = useRouter();
 
 const Stack = createNativeStackNavigator();
@@ -13,11 +14,21 @@ export default function Index() {
   return (
     <NavigationContainer independent={true}>
       <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={Home}   
-        options={{ title: 'Dashboard' }}
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ title: "Dashboard" }}
         />
-        <Stack.Screen name="Details" component={Details} />
-
+        <Stack.Screen
+          name="Details"
+          component={Details}
+          initialParams={{ itemId: 42 }}
+        />
+         <Stack.Screen
+          name="CreatePost"
+          component={CreatePostScreen}
+          options={{ title: "write mail" }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
